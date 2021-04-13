fd9db7636f99435368c5c9538573e46c31de3017
cromalde <cromalde@student.42roma.it>
2021-04-13 13:15:18 +0200

    cpp04 -- ex02

--- C++04/ex02/ISpaceMarine.hpp
+++ C++04/ex02/ISpaceMarine.hpp
@@ -1,12 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Squad.hpp                                          :+:      :+:    :+:   */
+/*   ISpaceMarine.hpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cromalde <cromalde@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/13 13:00:39 by cromalde          #+#    #+#             */
-/*   Updated: 2021/04/13 13:15:00 by cromalde         ###   ########.fr       */
+/*   Created: 2021/04/13 12:23:34 by cromalde          #+#    #+#             */
+/*   Updated: 2021/04/13 12:28:50 by cromalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef	ISPACEMARINE_HPP
+# define ISPACEMARINE_HPP
+
+class ISpaceMarine
+{
+	public:
+			virtual	~ISpaceMarine(void) {}
+			virtual ISpaceMarine* clone() const = 0;
+			virtual void battleCry() const = 0;
+			virtual void rangedAttack() const = 0;
+			virtual void meleeAttack() const = 0;
+};
+
+#endif
