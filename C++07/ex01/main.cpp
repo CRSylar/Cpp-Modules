1ae7ce466c8a01d7d0c8090df3facb4abc216333
cromalde <cromalde@student.42roma.it>
2021-04-17 10:02:11 +0200

    cpp07 -- ex01 -- finito

--- C++07/ex01/main.cpp
+++ C++07/ex01/main.cpp
@@ -6,22 +6,26 @@
 /*   By: cromalde <cromalde@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/16 16:40:35 by cromalde          #+#    #+#             */
-/*   Updated: 2021/04/16 16:44:44 by cromalde         ###   ########.fr       */
+/*   Updated: 2021/04/17 10:01:36 by cromalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "iter.hpp"
+#include "Iter.hpp"
 
-int		main(int ac, char **av)
+int		main(void)
 {
-	int i = 1;
-	if (ac < 2)
-		return 1;
-	while (av[i])
-	{
-		std::string str = av[i];
-		::iter(av[i], str.length(), &ft_print);
-		i++;
-	}
+	int i_arr[5] = {0, 1, 2, 3, 4};
+	bool b[2] = {false, true};
+	std::string str[5] = {"prova", "di", "funzionamento", "template", "con funzione"};
+
+	::iter(str, 5, &ft_print);
+	std::cout << std::endl;
+
+	::iter(b, 2, &ft_print);
+	std::cout << std::endl;
+
+	::iter(i_arr, 5, &ft_print);
+	std::cout << std::endl;
+
 	return 0;
 }
