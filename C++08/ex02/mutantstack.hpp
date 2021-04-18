7aacd79cf00c6f0b7012518d519ed5ffcf27bdb4
cromalde <cromalde@student.42roma.it>
2021-04-18 16:36:36 +0200

    cpp08 -- ex02 -- finito

--- C++08/ex02/mutantstack.hpp
+++ C++08/ex02/mutantstack.hpp
@@ -6,7 +6,7 @@
 /*   By: cromalde <cromalde@student.42roma.it>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/18 14:49:56 by cromalde          #+#    #+#             */
-/*   Updated: 2021/04/18 15:01:03 by cromalde         ###   ########.fr       */
+/*   Updated: 2021/04/18 16:08:26 by cromalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,31 @@
 # include <stack>
 
 template <typename T>
-class mutantstack : public std::stack
+class MutantStack : public std::stack<T>
 {
 	public:
-		mutantstack(void);
-		~mutantstack(void);
+		MutantStack(void);
+		MutantStack(const MutantStack<T>& src);
+		~MutantStack(void);
+
+		MutantStack<T>&	operator=(const MutantStack<T>& src);
+
+		typedef typename std::stack<T>::container_type::iterator				iterator;
+		typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
+
+		iterator					begin(void);
+		iterator					end(void);
+		const_iterator				begin(void) const;
+		const_iterator				end(void) const;
+		reverse_iterator			rbegin(void);
+		reverse_iterator			rend(void);
+		const_reverse_iterator		rbegin(void) const;
+		const_reverse_iterator		rend(void) const;
 	private:
-		/* data */
 };
 
-template <typename T>
-mutantstack<T>::mutantstack(void)
-{}
-
-template <typename T>
-mutantstack<T>::~mutantstack(void)
-{}
-
-
+# include "mutantstack.cpp"
 
 #endif
